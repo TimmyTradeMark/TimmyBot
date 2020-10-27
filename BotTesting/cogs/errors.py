f7303a3b6dbdb3a5aaa372c58862ce21899a5160
Kacper <stasikkacper1@gmail.com>
2020-10-27 09:13:43 -0500

    Update errors.py

--- BotTesting/cogs/errors.py
+++ BotTesting/cogs/errors.py
@@ -6,8 +6,11 @@ class ErrorHandler(commands.Cog):
         self.client = client
     
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        await ctx.send(error)
+    async def on_command_error(self, ctx, error): # https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.on_command_error
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send("This command does not exist!")
+        else:
+            pass
 
 def setup(client):
     client.add_cog(ErrorHandler(client))
